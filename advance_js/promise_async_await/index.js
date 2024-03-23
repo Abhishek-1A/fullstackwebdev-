1fcd006b24178d9f7990a534dd789e4cbe285798
Abhishek-A <kua77663@gmail.com>
2024-03-23 11:05:27 +0530

    sync and async js

---
+++ advance_js/promise_async_await/index.js
@@ -0,0 +1,10 @@
+console.log("iam forst synchronous js");
+console.log("iam second");
+
+// asynchronous in js
+
+setTimeout(() =>{
+    console.log("ASync JS");
+}, 4000);
+
+console.log("am");
---
+++ advance_js/promise_async_await/script.js
@@ -0,0 +1,25 @@
+function loadingData(){
+
+    setTimeout(() =>{
+        console.log("Loading data");
+    }, 3000)
+   
+}
+function Approavedata(){
+    console.log("Approave data");
+}
+function collectingData(){
+    setTimeout(() =>{
+        console.log("Loading data");
+    }, 2000)
+   
+    
+}
+function ApprovedData(){
+    console.log("Approved data");
+}
+
+loadingData();
+Approavedata();
+collectingData();
+Approavedata();
