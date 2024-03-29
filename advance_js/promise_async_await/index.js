4cd79181965090c111f95b3d73ec46f5c2ed326c
Abhishek-A <kua77663@gmail.com>
2024-03-29 12:42:13 +0530

    promise ka example

--- advance_js/promise_async_await/index.js
+++ advance_js/promise_async_await/index.js
@@ -12,23 +12,86 @@
 
 // callback
 
-function getData(dataId, getnextData) {
-    // 2s delay
-    setTimeout(()=>{
-        console.log("data",dataId);
-        if(getnextData){
-            getnextData();
-        }
-    }, 2000);
-}
+// function getData(dataId, getnextData) {
+//     // 2s delay
+//     setTimeout(()=>{
+//         console.log("data",dataId);
+//         if(getnextData){
+//             getnextData();
+//         }
+//     }, 2000);
+// }
 
-// this is callback hell
-getData(1, ()=>{
-    getData(2, ()=>{
-        getData(3, ()=>{
-            getData(4, ()=> {
+// // this is callback hell
+// getData(1, ()=>{
+//     getData(2, ()=>{
+//         getData(3, ()=>{
+//             getData(4, ()=> {
 
-            });
-        });
-    });
-});
+//             });
+//         });
+//     });
+// });
+
+
+
+
+// function printHello(){
+//     console.log("Hello Ninjas!");
+// }
+
+// function printMessage(){
+//     console.log("I'm a pro coder!")
+// }
+
+// function helper(callback){
+
+//     // Calling the callback() function
+//     callback();
+// }
+
+// // First call
+// helper(printHello);
+
+// // Second call
+// helper(printMessage);
+
+
+// promise
+
+// let promise= new Promise((resolve, reject) =>{
+//     console.log("I am promis");
+//     reject("some error occured")
+// });
+
+// function getData(dataId, getnextData) {
+//     return new Prommise((resole, reject) =>{
+//         setTimeout(()=>{
+//                     console.log("data",dataId);
+//                     resole("success");
+//                     if(getnextData){
+//                         getnextData();
+//                     }
+//                 }, 5000);
+//     });
+// }
+
+
+
+// promise example  :--> user ask for a num betw 0 to 9 if num is below 5 or above 5
+
+var ans= new Promise((resolve, reject) => {
+    var n= Math.floor(Math.random( ) * 10);
+
+    if(n<5) {
+        return resolve();
+    } else {
+        return reject();
+    }
+})
+ ans.then(()=> {
+    console.log("below");
+ })
+ .catch(() =>{
+    console.log("Above");
+ })
